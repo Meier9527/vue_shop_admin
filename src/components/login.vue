8e9c33780b33c83e94ead2ab221cec2a3419a4a2
meier9527 <18003094207@163.com>
2020-01-21 22:25:12 +0800

    login-box

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="login_container">
-    登录组件
+    <div class="login_box">
+
+    </div>
   </div>
 </template>
 
@@ -11,5 +13,16 @@ export default {}
 <style lang="less" scoped>
 .login_container{
 	background-color: #3CAFE5;
+	height: 100%;
+}
+.login_box{
+	width: 450px;
+	height: 350px;
+	background-color: #ddd;
+	border-radius:5px;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
 }
 </style>
